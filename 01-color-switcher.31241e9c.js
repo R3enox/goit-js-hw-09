3a9a3a0ff931c22126a4052a2794e23d6cfc24f7
R3enox <129979133+R3enox@users.noreply.github.com>
2023-08-29 18:13:21 +0000

    Deploying to gh-pages from @ R3enox/goit-js-hw-09@80b15d94a51aceb476765480ad9bdd4f0ea5d319 🚀

---
+++ 01-color-switcher.31241e9c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){timerID=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0}),1e3)}));e.addEventListener("click",(function(){clearInterval(timerID),t.style.pointerEvents="",t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.31241e9c.js.map
